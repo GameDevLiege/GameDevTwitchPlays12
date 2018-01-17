e4932a16f421dfd0e3097ea4e0b3e3d5bf24bc76
Unknown <neildidi33@gmail.com>
2018-01-17 17:32:49 +0100

    Last Commit of the day

---
+++ Assets/_GameDevTwitchPlays12/Teams/Team Twitch/Module 1/Scripts/General/Editor/Mockup_SequenceMessagesEditor.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace DidzNeil.ChatAPI
+{
+    [CustomEditor(typeof(Mockup_SequenceMessagesEditor))]
+    public class Mockup_SequenceMessagesEditor : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+            
+        }
+
+        private static void InputMessages(IEnumerable<Message> messages)
+        {
+
+            GUILayout.Label("MockUp Test ");
+
+            //EditorGUILayout.TextArea();
+            
+            EditorGUILayout.BeginHorizontal();
+            GUILayout.Label("UserName");
+            GUILayout.Label("Messages");
+            EditorGUILayout.EndHorizontal();
+
+      
+            EditorGUILayout.BeginVertical();
+            foreach (Message info in messages)
+            {
+                EditorGUILayout.BeginHorizontal();
+                GUILayout.TextField(info.GetUserName());
+                GUILayout.TextArea(info.GetMessage());
+                EditorGUILayout.EndHorizontal();
+            }
+            EditorGUILayout.EndVertical();
+
+        }
+    }
+}
+
