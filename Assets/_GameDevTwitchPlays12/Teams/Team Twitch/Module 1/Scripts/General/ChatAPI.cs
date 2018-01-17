200016d3647d7db7d54ef950e6cb080b237ad299
Technifutur <barudy.diego@gmail.com>
2018-01-17 17:30:40 +0100

    Branch SendToTwich

--- Assets/_GameDevTwitchPlays12/Teams/Team Twitch/Module 1/Scripts/General/ChatAPI.cs
+++ Assets/_GameDevTwitchPlays12/Teams/Team Twitch/Module 1/Scripts/General/ChatAPI.cs
@@ -28,7 +28,6 @@ namespace DidzNeil.ChatAPI
         #endregion
 
         public static bool IgnoreMockUp;
-        public delegate void MessageReceived(Message message);
         private static MessageReceived m_onMessageReceived;
        // public delegate void MessageSended(Message message);
        // private static MessageSended m_onMessageSended;
@@ -65,25 +64,23 @@ namespace DidzNeil.ChatAPI
 
 
         #region SEND INFO
-        public delegate void SendMessageTo(string user, Platform platform, Message msg);
-        public delegate void SendMessageToAll( Message msg);
         // public delegate void SendMessageToAllOnPlatform(Platform platform, Message msg);
 
 
 
-        public SendMessageTo _sendMessageToUser;
-        public SendMessageToAll _sendMessageEveryBody;
+        public static SendMessageTo _sendMessageToUser;
+        public static SendMessageToAll _sendMessageEveryBody;
 
-        public void AddListener(SendMessageTo user) { _sendMessageToUser += user; }
-        public void AddListener(SendMessageToAll allUser) { _sendMessageEveryBody += allUser; }
+        public static void AddGameToServerListener(SendMessageTo user) { _sendMessageToUser += user; }
+        public static void AddGameToServerListener(SendMessageToAll allUser) { _sendMessageEveryBody += allUser; }
 
-        public void SendMessageToUser(string user, Platform platform, Message msg) {
+        public static void SendMessageToUser(string user, Platform platform, Message msg) {
 
             if (_sendMessageToUser != null)
                 _sendMessageToUser(user, platform, msg);
 
         }
-        public void SendMessageToEveryUsers( Message msg)
+        public static void SendMessageToEveryUsers( Message msg)
         {
 
             if (_sendMessageEveryBody != null)
@@ -94,5 +91,9 @@ namespace DidzNeil.ChatAPI
         #endregion
 
     }
+
+    public delegate void MessageReceived(Message message);
+    public delegate void SendMessageTo(string user, Platform platform, Message msg);
+    public delegate void SendMessageToAll(Message msg);
 
 }
---
+++ Assets/_GameDevTwitchPlays12/Teams/Team Twitch/Module 1/Scripts/Plugs/TwitchIRC/SendToTwitch.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DidzNeil.ChatAPI;
+using System;
+
+public class SendToTwitch : MonoBehaviour {
+
+    // Use this for initialization
+    IEnumerator Start()
+    {
+        ChatAPI.AddGameToServerListener(SendMessage);
+        ChatAPI.AddGameToServerListener(SendMessageToAll);
+
+
+        yield return new WaitForSeconds(1);
+
+        ChatAPI.SendMessageToUser("jamscenter", Platform.Twitch, new Message("GameMaster", "bonjour toi", Message.GetCurrentTimeUTC(), Platform.Game));
+        ChatAPI.SendMessageToEveryUsers(new Message("GameMaster", "bonjour tous",Message.GetCurrentTimeUTC(),Platform.Game));
+
+
+    }
+
+    private void SendMessage(string user, Platform platform, Message msg)
+    {
+
+
+
+
+
+    }
+    private void SendMessageToAll(Message msg)
+    {
+
+
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+
+    
+}
