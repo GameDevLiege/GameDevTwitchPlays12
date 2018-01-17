13dc2b1971a9a8fff2b2f8964d64255fc41ce40b
Technifutur <neildidi33@gmail.com>
2018-01-17 10:50:55 +0100

    First commit, ChatAPI implémentation

---
+++ Assets/ChatAPIToConsole.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DidzNeil.ChatAPI;
+using System;
+
+public class ChatAPIToConsole : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        ChatAPI.AddListener(DisplayMessageWhenReceived);
+	}
+
+    private void DisplayMessageWhenReceived(Message message)
+    {
+        Debug.Log(message.GetUserName()+": "+ message.GetMessage());
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
