576a1a6c02461336eff8949e1e4fa31ea9fdce6c
Technifutur <neildidi33@gmail.com>
2018-01-17 12:53:19 +0100

    Demo for Chat to text in scene

---
+++ Assets/_GameDevTwitchPlays12/Teams/Team Twitch/Module 1/Editor/ChatAPiEditor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace DidzNeil.ChatAPI
+{
+    [CustomEditor(typeof(ChatAPIMono))]
+    public class ChatAPIEditor : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            DisplayLastMessages(ChatAPI.GetLastMessages(20));
+        }
+
+        private static void DisplayLastMessages(IEnumerable<Message> messages)
+        {
+            GUILayout.Label("Last 10 Messages");
+            foreach (Message msg in messages)
+            {
+
+                GUILayout.Label(">:"+msg.GetMessage());
+            }
+        }
+    }
+}
