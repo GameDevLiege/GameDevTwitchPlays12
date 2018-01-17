3a502cb4a89e950be4fe3377d843a4116c860bda
CorentinGengler <gengcor@gmail.com>
2018-01-17 10:43:22 +0100

    v1

---
+++ Assets/_GameDevTwitchPlays12/Teams/Team Physics/Module 1/Scripts/Territory.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Territory  : MonoBehaviour
+{
+
+    #region Public Members
+    
+    
+    //List<Player> m_listPlayerOnTerritory;
+    
+
+    #endregion
+
+
+    #region Public Void
+    public bool HasSpecial
+    {
+        get { return m_hasSpecial; }
+        set { m_hasSpecial = value; }
+    }
+    public bool IsWall
+    {
+        get { return m_isAWall; }
+        set { m_isAWall = value; }
+    }
+    public bool IsHQ
+    {
+        get { return m_isHQ; }
+        set { m_isHQ = value; }
+    }
+    public Color CurrentColor
+    {
+        get { return m_currentColor; }
+        set { m_currentColor = value; }
+    }
+    #endregion
+
+
+    #region System
+
+    void Awake () 
+    {
+		
+	}
+
+    #endregion
+
+    #region Private Void
+    private void OnTriggerEnter(Collider other)
+    {
+        
+    }
+    #endregion
+
+    #region Tools Debug And Utility
+
+    #endregion
+
+
+    #region Private And Protected Members
+    private bool m_hasSpecial;
+    private bool m_isAWall;
+    private bool m_isHQ;
+    private Color m_currentColor;
+    #endregion
+
+}
---
+++ Assets/_GameDevTwitchPlays12/Teams/Team Physics/Module 2/Scripts/PhysicsManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysicsManager  : MonoBehaviour
+{
+
+    #region Public Members
+    public int m_incomePerTerritory;
+    public float m_timeBetweenPayDay;
+    public int m_nbrXTerritories =33;
+    public int m_nbrYTerritories =33;
+    public GameObject m_TerritoryPrefab;
+    #endregion
+
+
+    #region Public Void
+
+    #endregion
+
+
+    #region System
+
+    void Start () 
+    {
+		
+	}
+    void Awake () 
+    {
+		
+	}
+	
+	void Update () 
+    {
+		
+	}
+
+    #endregion
+
+    #region Private Void
+    private void InitializeBoard()
+    {
+
+    }
+    #endregion
+
+    #region Tools Debug And Utility
+
+    #endregion
+
+
+    #region Private And Protected Members
+
+    #endregion
+
+}
