5850e42303a32df3eceb93a6337933a342a64e7d
Staces <staescedric@gmail.com>
2018-01-18 16:09:18 +0100

    CONSTRUCTION COMPRISE

--- Assets/_GameDevTwitchPlays12/Teams/Team Graphic Designer/Module 1/Scripts/CubeFlotte.cs
+++ Assets/_GameDevTwitchPlays12/Teams/Team Graphic Designer/Module 1/Scripts/CubeFlotte.cs
@@ -1,10 +1,35 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
 
-public class CubeFlotte : MonoBehaviour {
-    
-	void Start () {
-        //gameObject.transform.Translate();
-	}
+// Makes objects float up & down while gently spinning.
+public class CubeFlotte : MonoBehaviour
+{
+    // User Inputs
+    public float degreesPerSecond = 15.0f;
+    public float amplitude = 0.5f;
+    public float frequency = 1f;
+
+    // Position Storage Variables
+    Vector3 posOffset = new Vector3();
+    Vector3 tempPos = new Vector3();
+
+    // Use this for initialization
+    void Start()
+    {
+        // Store the starting position & rotation of the object
+        posOffset = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Spin object around Y-Axis
+        transform.Rotate(new Vector3(0f, Time.deltaTime * degreesPerSecond, 0f), Space.World);
+
+        // Float up/down with a Sin()
+        tempPos = posOffset;
+        tempPos.z += Mathf.Sin(Time.fixedTime * Mathf.PI * frequency) * amplitude;
+
+        transform.position = tempPos;
+    }
 }
