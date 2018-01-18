f415767f3a42a1bee5dad0768522e420e9ffe0f3
Staces <staescedric@gmail.com>
2018-01-18 12:46:22 +0100

    script cubeFlotte

---
+++ Assets/_GameDevTwitchPlays12/Teams/Team Graphic Designer/Module 1/Scripts/CubeFlotte.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeFlotte : MonoBehaviour {
+    
+	void Start () {
+        //gameObject.transform.Translate();
+	}
+}
