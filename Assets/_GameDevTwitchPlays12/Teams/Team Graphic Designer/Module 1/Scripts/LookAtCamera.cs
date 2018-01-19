08834e904db143ce64d040c48576c554eb6c6ee4
Staces <staescedric@gmail.com>
2018-01-19 11:25:53 +0100

    First Ping Reconnaissant Porteur Lunette

---
+++ Assets/_GameDevTwitchPlays12/Teams/Team Graphic Designer/Module 1/Scripts/LookAtCamera.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class LookAtCamera : MonoBehaviour {
+
+    private void Start()
+    {
+        m_rotation = transform.rotation;
+    }
+
+    void Update () {
+        transform.rotation = m_rotation;
+    }
+
+    private Quaternion m_rotation;
+}
---
+++ Assets/_GameDevTwitchPlays12/Teams/Team Graphic Designer/Module 1/Scripts/RotateGlasses.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateGlasses : MonoBehaviour {
+
+    public float m_speedRotation = 1f;
+
+    // Update is called once per frame
+    void Update () {
+       // transform.rotation += new Quaternion(transform.rotation.x, m_speedRotation * Time.deltaTime, transform.rotation.z, transform.rotation.w);
+	}
+}
