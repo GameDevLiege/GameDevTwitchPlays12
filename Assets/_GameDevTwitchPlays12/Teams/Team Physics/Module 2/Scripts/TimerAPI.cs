2814388dcd59fc19fcb1fcbee78534b9885e218c
Technifutur <neildidi33@gmail.com>
2018-01-19 10:42:51 +0100

    class timer todo

---
+++ Assets/_GameDevTwitchPlays12/Teams/Team Physics/Module 2/Scripts/TimerAPI.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using UniRx;
+
+
+public class TimerAPI : MonoBehaviour
+{
+    public float m_timeSpecial_1;
+    public Timer
+}
