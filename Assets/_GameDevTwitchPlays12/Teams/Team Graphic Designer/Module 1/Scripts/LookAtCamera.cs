af9ac3dfce4d23ccb19cd548bb6fe8afc0b61daa
Staces <staescedric@gmail.com>
2018-01-19 12:56:25 +0100

    Ping Finish

--- Assets/_GameDevTwitchPlays12/Teams/Team Graphic Designer/Module 1/Scripts/LookAtCamera.cs
+++ Assets/_GameDevTwitchPlays12/Teams/Team Graphic Designer/Module 1/Scripts/LookAtCamera.cs
@@ -2,21 +2,8 @@
 
 public class LookAtCamera : MonoBehaviour {
 
-    void Update () {
-        //transform.forward = -Camera.main.transform.forward;
-        /*Quaternion newRotation = Quaternion.LookRotation(Camera.main.transform.position - transform.position );
-        newRotation.x = 0.0f;
-        newRotation.y = 0.0f;
-        transform.rotation = Quaternion.Slerp(transform.rotation, newRotation, Time.deltaTime * 8);*/
-        /* Quaternion newRotation = Quaternion.LookRotation(Camera.main.transform.position, Vector3.forward);
-         newRotation.z = 0.0f;
-         //newRotation.y = 0.0f;
-         newRotation.x = 0.0f;
-         newRotation.w = 0.0f;
-         transform.rotation = newRotation;*/
+    void Start () {
 
-        //transform.LookAt(Camera.main.transform.position, Vector3.back);
-
-        transform.rotation = Quaternion.LookRotation(Camera.main.transform.position, Vector3.back);
+        transform.LookAt(Camera.main.transform.position, Vector3.back);
     }
 }
