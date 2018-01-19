ecf95541bcf447f2df039c5bf48f7d8979226e52
JeromeJ <jerome.jamart.42@gmail.com>
2018-01-19 10:31:19 +0100

    Loot dispatching integration 2

--- Assets/_GameDevTwitchPlays12/Scripts/GameManager12.cs
+++ Assets/_GameDevTwitchPlays12/Scripts/GameManager12.cs
@@ -48,15 +48,51 @@ public class GameManager12 : MonoBehaviour
         SpecialAPI.AddListener(HandleEvent);
 
 
-        //Special spe = new Special();
-        //spe.m_playerCharacter = new PlayerCharacter();
-        //spe.m_typeSpecial = Special.e_specialType.PARCHEMENT;
-        //SpecialAPI.NotifyNewSpecial(spe);
+        Special spe = new Special();
+        spe.m_playerCharacter = new PlayerCharacter() { PlayerName = "0 Test" };
+        spe.m_typeSpecial = Special.e_specialType.PARCHEMENT;
+        SpecialAPI.NotifyNewSpecial(spe);
     }
 
     private void HandleEvent(ISpecial special)
     {
-        // m_commandManager.Parse()
+        string[] userInfo = special.m_playerCharacter.PlayerName.Split(new char[] { ' ' }, 2);
+
+
+        DateTime unixStart = new DateTime(1970, 1, 1, 0, 0, 0, 0, System.DateTimeKind.Utc);
+        long timestamp = (DateTime.Now.ToUniversalTime() - unixStart).Ticks;
+
+
+        string state = "";
+
+        switch (special.m_typeSpecial)
+        {
+            case Special.e_specialType.PEBBLE:
+                break;
+            case Special.e_specialType.COINCHEST:
+                break;
+            case Special.e_specialType.GRENADES:
+                break;
+            case Special.e_specialType.SHOVEL:
+                break;
+            case Special.e_specialType.PARCHEMENT:
+                state = "STUN";
+                break;
+            case Special.e_specialType.STRAIN:
+                state = "STRAIN";
+                break;
+            case Special.e_specialType.GLASSES:
+                break;
+            default:
+                break;
+        }
+
+        state = ((CommandManager)m_commandManager).firstStateCharacter + state;
+
+
+
+
+        m_commandManager.Parse(userInfo[1], Int32.Parse(userInfo[0]), state, timestamp);
     }
 
     private void HandleMessage(Message message)
