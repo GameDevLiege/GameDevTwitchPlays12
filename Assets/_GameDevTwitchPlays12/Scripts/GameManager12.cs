e81ea784f50a17287d05bf92d68a038856188661
JeromeJ <jerome.jamart.42@gmail.com>
2018-02-05 10:12:53 +0100

    Code Cleanup

--- Assets/_GameDevTwitchPlays12/Scripts/GameManager12.cs
+++ Assets/_GameDevTwitchPlays12/Scripts/GameManager12.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,10 +6,8 @@ using UnityEngine;
 using UnityEditor;
 #endif
 
-
 using GameManager;
 using DidzNeil.ChatAPI;
-
 
 public class FakeInput : IInput
 {
@@ -20,15 +17,13 @@ public class FakeInput : IInput
     }
 }
 
-
 public class GameManager12 : MonoBehaviour
 {
     #region Public Members
 
-    public IGameEngine m_gameEngine;
-
     public IInput m_input = new FakeInput();
 
+    public IGameEngine m_gameEngine;
     public ICommandManager m_commandManager;
 
     #endregion
@@ -45,78 +40,13 @@ public class GameManager12 : MonoBehaviour
         m_gameEngine = GetComponent<PhysicsManager>();
     }
 
-#if UNITY_EDITOR
-    [MenuItem("GDL-Twitch12/Stun Neil %t")]
-    public static void test()
-    {
-        Special spe = new Special();
-        spe.m_playerCharacter = new PlayerCharacter() { PlayerName = "0 Neil" };
-        spe.m_typeSpecial = Special.e_specialType.PARCHEMENT;
-        SpecialAPI.NotifyNewSpecial(spe);
-
-        string ticks = "?";
-        try
-        {
-            ticks = GameObject.Find("PManager").GetComponent<CommandManager>().stunMult.ToString();
-        }
-        catch(Exception)
-        {
-
-        }
-
-        Debug.LogWarning("Stunning Neil for " +  ticks + " ticks!");
-    }
-#endif
-
-
     protected void Start()
     {
+        // Either lead to Nothing, Feedback user or influence the game.
         ChatAPI.AddListener(HandleMessage);
+
+        // Item pickups influences the cooldown on the CommandManager
         SpecialAPI.AddListener(HandleEvent);
-
-
-        
-    }
-
-    private void HandleEvent(ISpecial special)
-    {
-        string[] userInfo = special.m_playerCharacter.PlayerName.Split(new char[] { ' ' }, 2);
-
-
-        DateTime unixStart = new DateTime(1970, 1, 1, 0, 0, 0, 0, System.DateTimeKind.Utc);
-        long timestamp = (DateTime.Now.ToUniversalTime() - unixStart).Ticks;
-
-
-        string state = "";
-
-        switch (special.m_typeSpecial)
-        {
-            case Special.e_specialType.PEBBLE:
-                break;
-            case Special.e_specialType.COINCHEST:
-                break;
-            case Special.e_specialType.GRENADES:
-                break;
-            case Special.e_specialType.SHOVEL:
-                break;
-            case Special.e_specialType.PARCHEMENT:
-                state = "STUN";
-                break;
-            case Special.e_specialType.STRAIN:
-                state = "STRAIN";
-                break;
-            case Special.e_specialType.GLASSES:
-                break;
-            default:
-                break;
-        }
-
-        state = ((CommandManager)m_commandManager).firstStateCharacter + state;
-
-
-
-
-        m_commandManager.Parse(userInfo[1], Int32.Parse(userInfo[0]), state, timestamp);
     }
 
     private void HandleMessage(Message message)
@@ -155,9 +85,40 @@ public class GameManager12 : MonoBehaviour
         }
     }
 
-    private void Update()
+    private void HandleEvent(ISpecial special)
     {
+        string[] userInfo = special.m_playerCharacter.PlayerName.Split(new char[] { ' ' }, 2);
 
+        DateTime unixStart = new DateTime(1970, 1, 1, 0, 0, 0, 0, System.DateTimeKind.Utc);
+        long timestamp = (DateTime.Now.ToUniversalTime() - unixStart).Ticks;
+
+        string state = "";
+
+        switch (special.m_typeSpecial)
+        {
+            case Special.e_specialType.PEBBLE:
+                break;
+            case Special.e_specialType.COINCHEST:
+                break;
+            case Special.e_specialType.GRENADES:
+                break;
+            case Special.e_specialType.SHOVEL:
+                break;
+            case Special.e_specialType.PARCHEMENT:
+                state = "STUN";
+                break;
+            case Special.e_specialType.STRAIN:
+                state = "STRAIN";
+                break;
+            case Special.e_specialType.GLASSES:
+                break;
+            default:
+                break;
+        }
+
+        state = ((CommandManager)m_commandManager).firstStateCharacter + state;
+
+        m_commandManager.Parse(userInfo[1], Int32.Parse(userInfo[0]), state, timestamp);
     }
 
     #endregion
@@ -167,6 +128,29 @@ public class GameManager12 : MonoBehaviour
     #endregion
 
     #region Tools Debug and Utility
+
+#if UNITY_EDITOR
+    [MenuItem("GDL-Twitch12/Stun Player %t")]
+    public static void StunPlayer()
+    {
+        Special spe = new Special
+        {
+            m_playerCharacter = new PlayerCharacter() { PlayerName = "0 Neil" },
+            m_typeSpecial = Special.e_specialType.PARCHEMENT
+        };
+
+        SpecialAPI.NotifyNewSpecial(spe);
+
+        string ticks = "?";
+        try
+        {
+            ticks = GameObject.Find("PManager").GetComponent<CommandManager>().stunMult.ToString();
+        }
+        catch (Exception) { }
+
+        Debug.LogWarning("Stunning Neil for " + ticks + " ticks!");
+    }
+#endif
 
     #endregion
 
