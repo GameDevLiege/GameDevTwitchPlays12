6827e9431ac380d031c361511704667051cddd98
Staces <staescedric@gmail.com>
2018-02-07 10:47:52 +0100

    ObjectsFollow

--- Assets/_GameDevTwitchPlays12/Teams/Team Graphic Designer/Module 1/Scripts/GlassesFollow.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-public class GlassesFollow : MonoBehaviour {
-
-	public void GlassesFollowCharacter(Vector3 position)
-    {
-        transform.position = new Vector3(position.x,position.y,transform.position.z);
-    }
-}
