d96f5fbbb11805fa8f1a8ffab365084c9518a400
Rygy <remy.grandry95@gmail.com>
2018-02-07 17:05:50 +0100

    fixes nullable

--- Assets/_GameDevTwitchPlays12/Scripts/Commands&States/CommandManager.cs
+++ Assets/_GameDevTwitchPlays12/Scripts/Commands&States/CommandManager.cs
@@ -31,126 +31,129 @@ public class CommandManager : DualBehaviour, ICommandManager
         string userID = _plateform + " " + _username;
         _message = _message.ToUpper();
 
-        if (StartAsCommand(_message)) 
+        if (!MessageIsNull(_message))
         {
-            if (CommandIsValid(_message))
+            if (StartAsCommand(_message))
             {
-                string[] splitedMesage = SplitMessage(_message);
-
-                if (_message.Equals(firstCommmandCharacter + "JOIN"))
+                if (CommandIsValid(_message))
                 {
-                    if (!userDataBase.ContainsKey(userID))
+                    string[] splitedMesage = SplitMessage(_message);
+
+                    if (_message.Equals(firstCommmandCharacter + "JOIN"))
                     {
-                        if (userDataBase.Count < maxPlayer)
+                        if (!userDataBase.ContainsKey(userID))
                         {
-                            userDataBase.Add(userID, new PlayerCTRL(userID, this));
-                            gameManager.DoCommand(_username, _plateform, new Command(_message, false));
+                            if (userDataBase.Count < maxPlayer)
+                            {
+                                userDataBase.Add(userID, new PlayerCTRL(userID, this));
+                                gameManager.DoCommand(_username, _plateform, new Command(_message, false));
+                            }
+                            else
+                            {
+                                gameManager.DoCommand(_username, _plateform, new Command("Maximum number of players reached!", true));
+                            }
                         }
                         else
                         {
-                            gameManager.DoCommand(_username, _plateform, new Command("Le nombre maximum de joueur est atteint", true));
+                            gameManager.DoCommand(_username, _plateform, new Command("You already joined the game", true));
+                        }
+                    }
+                    else if (!userDataBase.ContainsKey(userID))
+                    {
+                        gameManager.DoCommand(_username, _plateform, new Command("Please first join the game using \"" + firstCommmandCharacter + "JOIN\" command.", true));
+                    }
+                    else if (userDataBase[userID].states.ContainsKey("FIGHT"))
+                    {
+                        if (userDataBase[userID].StateIsActive("FIGHT", _time))
+                        {
+                            gameManager.DoCommand(_username, _plateform, new Command("You're busy fighting right now!", true));
+                        }
+                    }
+                    else if (userDataBase[userID].states.ContainsKey("STUN"))
+                    {
+                        if (userDataBase[userID].StateIsActive("STUN", _time))
+                        {
+                            gameManager.DoCommand(_username, _plateform, new Command("You can't do anything right now because you're still STUN.", true));
+                        }
+                    }
+                    else if (userDataBase[userID].states.ContainsKey("MOVE"))
+                    {
+                        if (userDataBase[userID].StateIsActive("MOVE", _time))
+                        {
+                            gameManager.DoCommand(_username, _plateform, new Command("You're busy moving right now!", true));
+                        }
+                    }
+                    else if (userDataBase[userID].states.ContainsKey("SPRAIN") && (_message.Equals(firstCommmandCharacter + "DIG")))
+                    {
+                        if (userDataBase[userID].StateIsActive("SPRAIN", _time))
+                        {
+                            gameManager.DoCommand(_username, _plateform, new Command("You can't move now because you have a sprain!", true));
+                        }
+                    }
+                    else if (!Cooldown(_time, userID))
+                    {
+                        gameManager.DoCommand(_username, _plateform, new Command("Please wait for your cooldown to be over!", true));
+                    }
+                    else if (splitedMesage.Length == 2)
+                    {
+                        if (ArgsIsValid(splitedMesage[1]))
+                        {
+                            int number;
+                            int.TryParse(splitedMesage[1], out number);
+                            userDataBase[userID].AddState("MOVE", (_time + (number * cd)));
+
+                            StartCoroutine(Iteration(_username, _plateform, new Command(splitedMesage[0], false), number, userID));
+                        }
+                        else
+                        {
+                            gameManager.DoCommand(_username, _plateform, new Command("Invalid argument", true));
                         }
                     }
                     else
                     {
-                        gameManager.DoCommand(_username, _plateform, new Command("Vous avez déja rejoins la partie", true));
-                    }
-                }
-                else if (!userDataBase.ContainsKey(userID))
-                {
-                    gameManager.DoCommand(_username, _plateform, new Command("Veuillez d'abord rejoindre la partie a l'aide de la commande " + firstCommmandCharacter + "JOIN", true));
-                }
-                else if (userDataBase[userID].states.ContainsKey("FIGHT"))
-                {
-                    if (userDataBase[userID].StateIsActive("FIGHT", _time))
-                    {
-                        gameManager.DoCommand(_username, _plateform, new Command("Vous etes en train de combattre", true));
-                    }
-                }
-                else if (userDataBase[userID].states.ContainsKey("STUN"))
-                {
-                    if (userDataBase[userID].StateIsActive("STUN", _time))
-                    {
-                        gameManager.DoCommand(_username, _plateform, new Command("Vous ne pouvez pas effectuer d'action car vous êtes STUN", true));
-                    }
-                }
-                else if (userDataBase[userID].states.ContainsKey("MOVE"))
-                {
-                    if (userDataBase[userID].StateIsActive("MOVE", _time))
-                    {
-                        gameManager.DoCommand(_username, _plateform, new Command("Vous êtes en cours de mouvement", true));
-                    }
-                }
-                else if (userDataBase[userID].states.ContainsKey("SPRAIN") && (_message.Equals(firstCommmandCharacter + "DIG")))
-                {
-                    if (userDataBase[userID].StateIsActive("SPRAIN", _time))
-                    {
-                        gameManager.DoCommand(_username, _plateform, new Command("Vous ne pouvez pas creuser car vous vous êtes fais une entorse", true));
-                    }
-                }
-                else if (!Cooldown(_time, userID))
-                {
-                    gameManager.DoCommand(_username, _plateform, new Command("Le cooldown entre 2 commandes n'est pas terminé", true));
-                }
-                else if (splitedMesage.Length == 2)
-                {
-                    if (ArgsIsValid(splitedMesage[1]))
-                    {
-                        int number;
-                        int.TryParse(splitedMesage[1], out number);
-                        userDataBase[userID].AddState("MOVE", (_time + (number * cd)));
-
-                        StartCoroutine(Iteration(_username, _plateform, new Command(splitedMesage[0], false), number, userID));
-                    }
-                    else
-                    {
-                        gameManager.DoCommand(_username, _plateform, new Command("Argument invalide", true));
+                        userDataBase[userID].time = _time;
+                        splitedMesage[0] = ParseCommand(splitedMesage[0]);
+                        gameManager.DoCommand(_username, _plateform, new Command(splitedMesage[0], false));
                     }
                 }
                 else
                 {
-                    userDataBase[userID].time = _time;
-                    splitedMesage[0] = ParseCommand(splitedMesage[0]);
-                    gameManager.DoCommand(_username, _plateform, new Command(splitedMesage[0], false));
+                    gameManager.DoCommand(_username, _plateform, new Command("Your command is invalid!", true));
                 }
             }
-            else
+            else if (StartAsState(_message))
             {
-                gameManager.DoCommand(_username, _plateform, new Command("Votre commande est invalide", true));
-            }
-        }
-        else if (StartAsState(_message))
-        {
-            if (StateIsValid(_message))
-            {
-                switch (_message)
+                if (StateIsValid(_message))
                 {
-                    case "?STUN":
-                        userDataBase[userID].AddStun(_time);
-                        break;
-                    case "?SPRAIN":
-                        userDataBase[userID].AddSprain(_time);
-                        break;
-                    case "?FIGHT":
-                        if (!userDataBase[userID].StateIsActive("FIGHT", _time))
-                        {
-                            userDataBase[userID].AddFight(_time);
-                        }
-                        else
-                        {
-                            int randomPos = UnityEngine.Random.Range(0, 3);
-                            gameManager.DoCommand(_username, _plateform, new Command(movementCommand[randomPos], false));
-                        }
-                        break;
-                    default:
-                        break;
+                    switch (_message)
+                    {
+                        case "?STUN":
+                            userDataBase[userID].AddStun(_time);
+                            break;
+                        case "?SPRAIN":
+                            userDataBase[userID].AddSprain(_time);
+                            break;
+                        case "?FIGHT":
+                            if (!userDataBase[userID].StateIsActive("FIGHT", _time))
+                            {
+                                userDataBase[userID].AddFight(_time);
+                            }
+                            else
+                            {
+                                int randomPos = UnityEngine.Random.Range(0, 3);
+                                gameManager.DoCommand(_username, _plateform, new Command(movementCommand[randomPos], false));
+                            }
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                else
+                {
+                    gameManager.DoCommand(_username, _plateform, new Command("Invalid state input, ignored", false));
                 }
             }
-            else
-            {
-                gameManager.DoCommand(_username, _plateform, new Command("Invalid state input, ignored", false));
-            }
-        }
+        } 
     }
     #endregion
 
@@ -206,6 +209,15 @@ public class CommandManager : DualBehaviour, ICommandManager
     private bool StartAsCommand(string _message)
     {
         return _message[0] == firstCommmandCharacter;
+    }
+
+    private bool MessageIsNull(string _message)
+    {
+        if (_message == null)
+        {
+            return true;
+        }
+        return false;
     }
 
     private bool CommandIsValid(string _message)
