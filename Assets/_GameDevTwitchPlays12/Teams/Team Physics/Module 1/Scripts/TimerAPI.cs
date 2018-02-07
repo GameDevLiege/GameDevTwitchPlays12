58cbb1965b7b36f35c40d289af3c67492980f6a8
Unknown <barudy.diego@gmail.com>
2018-02-07 21:52:57 +0100

    Add peeble Stuff to test and refactoring of PlayerAction in PlayerManager

--- Assets/_GameDevTwitchPlays12/Teams/Team Physics/Module 1/Scripts/TimerAPI.cs
+++ Assets/_GameDevTwitchPlays12/Teams/Team Physics/Module 1/Scripts/TimerAPI.cs
@@ -1,43 +1,109 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
-using Homebrew;
+﻿using UnityEngine;
 
 
-public class TimerAPI : MonoBehaviour
+public class TimerAPI
 {
-    public delegate void MyTimer(bool end,Faction faction);
+    public delegate void MyTimer(bool end);
+    public delegate void MyTerritoryTimer(bool endLock);
+    public delegate void MyEndGameTimer(bool endGame);
+    public delegate void MyEndGameGlassTimer(bool endGame);
     private static MyTimer m_onTimer;
-
-
-    public static void AddListener(MyTimer timer)
+    private static MyTerritoryTimer m_onTerritoryTimer;
+    private static MyEndGameTimer m_onEndGameTimer;
+    private static MyEndGameGlassTimer m_onEndGameGlassTimer;
+    #region Listener
+    public static void AddTimerListener(MyTimer timer)
     {
         m_onTimer += timer;
 
     }
-    public static void RemoveListener(MyTimer timer)
+    public static void RemoveTimeListener(MyTimer timer)
     {
         m_onTimer -= timer;
 
     }
 
+    public static void AddTerritoryTimerListener(MyTerritoryTimer timer)
+    {
+        m_onTerritoryTimer += timer;
 
+    }
+    public static void RemoveTerritoryTimerListener(MyTerritoryTimer timer)
+    {
+        m_onTerritoryTimer -= timer;
+
+    }
+
+    public static void AddEndGameTimerListener(MyEndGameTimer timer)
+    {
+        m_onEndGameTimer += timer;
+
+    }
+    public static void RemoveEndGameTimeListener(MyEndGameTimer timer)
+    {
+        m_onEndGameTimer -= timer;
+
+    }
+    public static void AddEndGameGlassTimerListener(MyEndGameGlassTimer timer)
+    {
+        m_onEndGameGlassTimer += timer;
+
+    }
+    public static void RemoveEndGameGlassTimeListener(MyEndGameGlassTimer timer)
+    {
+        m_onEndGameGlassTimer -= timer;
+
+    }
+    #endregion
+    #region Notify
     public static void NotifyEndTimer()
     {
-        m_onTimer(true,null);
+        if (m_onTimer !=null)
+        m_onTimer(true);
     }
-   
-    public static Timer LaunchTimer(float seconds ) {
 
-        Timer timer = new Timer(seconds, NotifyEndTimer);
-        
-        return timer;
+    public static void NotifyEndGame()
+    {
+        m_onEndGameTimer(true);
     }
-    
 
-    
+    public static void NotifyEndGameGlass()
+    {
+        m_onEndGameGlassTimer(true);
+    }
+    public static void NotifyTerritoryUnlock()
+    {
+        m_onTerritoryTimer(true);
+    }
+
+
+    #endregion
+
+
+    public static void LaunchTimer(float seconds, Timer timer)
+    {
+        timer.LoadTimer(seconds, NotifyEndTimer);
+    }
+
+    public static void LaunchGameTimer(float seconds, Timer timer)
+    {
+
+        timer.LoadTimer(seconds, NotifyEndGame);
+
+    }
+
+    public static void LaunchGlassTimer(float seconds, Timer timer)
+    {
+
+        timer.LoadTimer(seconds, NotifyEndGameGlass);
+    }
+    public static void LaunchTerritoryLockTimer(float seconds, Timer timer)
+    {
+        timer.LoadTimer(seconds, NotifyTerritoryUnlock);
+    }
+
 
 
 }
+
 
