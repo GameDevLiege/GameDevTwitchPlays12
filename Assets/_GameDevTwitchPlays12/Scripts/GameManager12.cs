15b8fc9a8c86a6ad9b16e1d2335b7d0706e1c689
FrançoisM <600@technifutur.be>
2018-02-07 17:13:05 +0100

    integration : debug

--- Assets/_GameDevTwitchPlays12/Scripts/GameManager12.cs
+++ Assets/_GameDevTwitchPlays12/Scripts/GameManager12.cs
@@ -12,7 +12,7 @@ using DidzNeil.ChatAPI;
 public class GameManager12 : MonoBehaviour
 {
     #region Public Members    
-    public bool m_debug = true;
+    public bool m_debug = false;
 
     public ICommandManager m_commandManager;
     public PhysicsManager m_physicsManager;
