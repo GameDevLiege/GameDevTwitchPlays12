1940c680433b7046b9294580eba7e4b372544841
Staces <staescedric@gmail.com>
2018-02-07 12:48:28 +0100

    Effects

---
+++ Assets/_GameDevTwitchPlays12/Teams/Team Graphic Designer/Module 1/Scripts/TEST_EFFECT.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class TEST_EFFECT : MonoBehaviour {
+
+    public GameObject m_player1;
+    public GameObject m_player2;
+
+    public GameObject m_effect;
+
+
+    private void OnGUI()
+    {
+        Transform effect = m_effect.transform;
+
+        if (GUI.Button(new Rect(new Vector2(0, 0), new Vector2(100f, 50f)), "Glasses For Player 1"))
+            ObjectsFollow.FollowCharacter(effect, m_player1.transform.position);
+        if (GUI.Button(new Rect(new Vector2(0, 50f), new Vector2(100f, 50f)), "Glasses For Player 2"))
+            ObjectsFollow.FollowCharacter(effect, m_player2.transform.position);
+    }
+}
