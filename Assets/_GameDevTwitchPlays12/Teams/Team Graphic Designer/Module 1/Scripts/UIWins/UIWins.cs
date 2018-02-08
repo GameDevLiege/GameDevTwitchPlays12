ec703312fb12f9302e061edfb0a842c9a7b80922
Staces <staescedric@gmail.com>
2018-02-08 11:30:21 +0100

    UIWins

--- Assets/_GameDevTwitchPlays12/Teams/Team Graphic Designer/Module 1/Scripts/UIWins/UIWins.cs
+++ Assets/_GameDevTwitchPlays12/Teams/Team Graphic Designer/Module 1/Scripts/UIWins/UIWins.cs
@@ -1,16 +1,33 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class UIWins : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public GameObject m_Helmet;
+    public Text m_TeamName;
+    public Canvas m_CanvasUIWins;
+
+    private static GameObject Helmet;
+    private static Text TeamName;
+    private static Canvas CanvasUIWins;
+
+    public void Awake()
+    {
+        Helmet = m_Helmet;
+        TeamName = m_TeamName;
+        CanvasUIWins = m_CanvasUIWins;
+    }
+
+    public UIWins(string teamName, Color teamColor)
+    {
+        teamColor.a = 100;
+        Helmet.GetComponent<Renderer>().material.color = teamColor;
+        TeamName.text = teamName;
+        TeamName.color = teamColor;
+    }
+
+    public void ActiveUIWins(bool activation)
+    {
+        CanvasUIWins.gameObject.SetActive(activation);
+    }
 }
